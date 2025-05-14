950ecca2ee5b1c41f9384b38900f54e1e0fe20e9
Optimus <alisalahi19951995@gmail.com>
2025-05-15 02:03:20 +0330

    create date picker for profile page

--- src/components/common/InputDatePicker.tsx
+++ src/components/common/InputDatePicker.tsx
@@ -34,6 +34,9 @@ const generateYears = () => {
 };
 
 export default function JalaliDatePicker() {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  //
+  //
   const [year, setYear] = useState<string | undefined>();
   const [month, setMonth] = useState<string | undefined>();
   const [day, setDay] = useState<string | undefined>();
@@ -50,50 +53,67 @@ export default function JalaliDatePicker() {
   }, [year, month]);
 
   return (
-    <div className="flex items-center justify-between max-w-[420px] h-14 border border-neutral-text-200 rounded-lg">
+    <div
+      onClick={() => setIsFocused(!isFocused)}
+      className={`flex items-center justify-between max-w-[420px] h-14 border border-neutral-text-200 rounded-lg ${
+        isFocused && "border-primary "
+      } relative`}
+    >
+      <span className=" absolute right-4 -top-3 bg-neutral-white z-10 text-neutral-text-400 px-2">
+        تاریخ میلادی
+      </span>
       {/* Year */}
       <Select value={year} onValueChange={setYear}>
-        <SelectTrigger className=" border-none [&>svg]:hidden">
+        <SelectTrigger className="border-none focus:outline-none focus:ring-0 focus:ring-offset-0  flex items-center justify-center gap-2">
           <SelectValue placeholder="سال" />
-          <ChevronDown />
+          <ChevronDown className="opacity-80" size={20} />
         </SelectTrigger>
         <SelectContent className="max-h-[350px]">
           {generateYears().map((y) => (
-            <SelectItem key={y} value={y}>
-              {y}
+            <SelectItem
+              key={y}
+              value={y}
+              className="text-center hover:bg-primary-100"
+            >
+              {convertEnToFaNumbers(y)}
             </SelectItem>
           ))}
         </SelectContent>
       </Select>
-
+      <div className=" border h-2/3 " />
       {/* Month */}
       <Select value={month} onValueChange={setMonth}>
-        <SelectTrigger className=" border-none outline-none [&>svg]:hidden">
-          <SelectValue placeholder="ماه" />
-          <ChevronDown />
+        <SelectTrigger className="border-none focus:outline-none focus:ring-0 focus:ring-offset-0 flex items-center justify-center gap-2 ">
+          <SelectValue placeholder="ماه" className=" absolute" />
+          <ChevronDown className="opacity-80" size={20} />
         </SelectTrigger>
         <SelectContent className="max-h-[350px]">
           {months.map((m, i) => (
-            <SelectItem key={i} value={m}>
+            <SelectItem
+              key={i}
+              value={m}
+              className="text-right hover:bg-primary-100"
+            >
               {m}
             </SelectItem>
           ))}
         </SelectContent>
       </Select>
+      <div className=" border h-2/3 " />
 
       {/* Day */}
       <Select value={day} onValueChange={setDay}>
         <SelectTrigger
           disabled={!year || !month}
-          className=" border-none outline-none  [&>svg]:hidden"
+          className=" border-none focus:outline-none focus:ring-0 focus:ring-offset-0  flex items-center justify-center gap-2"
         >
           <SelectValue placeholder="روز" />
-          <ChevronDown />
+          <ChevronDown className="opacity-80" size={20} />
         </SelectTrigger>
         <SelectContent className="max-h-[350px]">
           {daysInMonth.map((d) => (
             <SelectItem key={d} value={d.toString()}>
-              {d}
+              {convertEnToFaNumbers(d)}
             </SelectItem>
           ))}
         </SelectContent>
--- src/pages/profile/components/Account.tsx
+++ src/pages/profile/components/Account.tsx
@@ -5,7 +5,7 @@ export default function Account() {
   return (
     <div>
       <div>
-        <div className=" w-full min-h-[185px] bg-neutral-white border border-neutral-400 rounded-lg px-3 py-2 ">
+        <div className=" flex flex-col gap-7 w-full min-h-[185px] bg-neutral-white border border-neutral-400 rounded-lg px-3 py-2 ">
           <InputForm label="نام به فارسی" placeholder="نام خود را وارد کنید" />
           <JalaliDatePicker />
         </div>
