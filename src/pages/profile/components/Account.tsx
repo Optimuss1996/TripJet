9404998029943b4a2e71079a3629f3b24ee9e372
Optimus <alisalahi19951995@gmail.com>
2025-05-17 12:15:03 +0330

    error fix typescript swiper slider

--- src/pages/profile/components/Account.tsx
+++ src/pages/profile/components/Account.tsx
@@ -1,17 +1,37 @@
 import { useAuth } from "@/providers/AuthProvider";
-import UserInfoSection from "./UserInfoSection";
-import PassportInfoSection from "./PassportInfoSection";
-import BankInfoSection from "./BankInfoSection";
+import { supabase } from "@/lib/supabaseClient";
+import { Users } from "@/types/types";
+import { useState, useEffect } from "react";
 
 export default function Account() {
-  const { user, userProfile } = useAuth();
-  console.log("userProfile", userProfile);
-  console.log("user", user);
+  const { userProfile } = useAuth();
+  const [dataUser, setDataUser] = useState<Users | null>(null);
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", "a0c95e9f-3c28-480e-984f-780d65d6a045")
+        .single();
+
+      if (error) {
+        console.error("خطا در دریافت اطلاعات کاربر:", error);
+      } else {
+        setDataUser(data as Users);
+      }
+    };
+
+    fetchUserProfile();
+  }, []);
+
+  console.log("userProfile از context:", userProfile);
+  console.log("dataUser از useEffect:", dataUser);
+
   return (
-    <div className=" flex flex-col gap-6 w-full">
-      {/* <UserInfoSection userProfile={userProfile!} />
-      <PassportInfoSection userProfile={userProfile!} />
-      <BankInfoSection userProfile={userProfile!} /> */}
+    <div className="flex flex-col gap-6 w-full">
+      {/* اینجا فقط نمایش می‌دیم تا مطمئن شیم */}
+      <pre>{JSON.stringify(dataUser, null, 2)}</pre>
     </div>
   );
 }
